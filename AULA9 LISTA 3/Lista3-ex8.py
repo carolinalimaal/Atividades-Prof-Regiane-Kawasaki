43fd0a39ffbad8b478e2dae14914b1dc2ff6a970
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2023-05-22 09:30:46 -0300

    Update Lista3-ex8.py

--- AULA9 LISTA 3/Lista3-ex8.py
+++ AULA9 LISTA 3/Lista3-ex8.py
@@ -1,6 +1,14 @@
-num1 = int(input('Insira o 1º número: '))
-num2 = int(input('Insira o 2º número: '))
-div1 = div2 = 0
+num1 = int(input('Insira o 1º valor: '))
+num2 = int(input('Insira o 2º valor: '))
 
-while div1 != 1:
-    
+if num1 > num2:
+    maior = num1
+else:
+    maior = num2
+
+mmc = maior
+
+while (mmc % num1) != 0 or (mmc % num2) != 0:
+    mmc += maior
+
+print(f'O MMC entre {num1} e {num2} é {mmc}')
