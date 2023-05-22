9a538407776284c89f96bf7bad486fa0e4824b3d
Carolina de Lima Alves <129516662+carolinalimaal@users.noreply.github.com>
2023-05-22 09:32:43 -0300

    Update Lista3-ex12.py

--- AULA9 LISTA 3/Lista3-ex12.py
+++ AULA9 LISTA 3/Lista3-ex12.py
@@ -1,11 +1,11 @@
 from random import randint
-vetor = [0]*10
+vetor = [0]*50
 soma = dp = 0
-for i in range(0,10):
+for i in range(0,50):
     vetor[i] = randint(1,10)
     soma += vetor[i]
-    dp += (vetor[i] - (soma/10))**2
-
+for i in range(0,5):
+    dp += (vetor[i]-(soma/50))**2  / 50
 print(f'LISTA DAS ALTURAS: {vetor}')
-print(f'A média das alturas é {soma/10}')
-print(f'O desvio padrão das alturas é {(dp/10)**(1/2)}')
+print(f'A média das alturas é {soma/50}')
+print(f'O desvio padrão das alturas é {(dp)**(1/2)}')
